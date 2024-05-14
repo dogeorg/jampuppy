d4cd8160fb0704e309c793cdee3c644e4b8dc361
Raffe <andrew@dogecoin.com>
2024-05-14 14:16:25 +1000

    Allow -d flag, resolve relative paths (cwd by default)

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log"
 	"os"
+	"path"
+	"strings"
 
 	flag "github.com/spf13/pflag"
 )
@@ -11,6 +13,7 @@ import (
 // net/http FileServer
 
 type Config struct {
+	Dir     string
 	Port    int
 	Host    string
 	Proxy   []Proxy
@@ -27,15 +30,24 @@ func main() {
 	flag.StringVarP(&config.Host, "host", "h", config.Host, "Listen interface")
 	flag.StringArrayVarP(&proxy, "proxy", "r", proxy, "Reverse Proxy: '/thing http://localhost:8085/thing' (one or more)")
 	flag.BoolVarP(&config.Verbose, "verbose", "v", config.Verbose, "Colour prints ingoing/outgoing requests for debugging")
+	flag.StringVarP(&config.Dir, "dir", "d", config.Dir, "Directory to serve")
 	flag.Parse()
 	config.Proxy = parseProxy(proxy)
+	if !strings.HasPrefix(config.Dir, "/") {
+		cwd, err := os.Getwd()
+		if err != nil {
+			cwd = "."
+		}
+		config.Dir = path.Join(cwd, config.Dir)
+	}
 	args := flag.Args()
 	if len(args) > 0 {
-		log.Printf("unrecognised arguments: %v\n", args)
+		log.Printf("unexpected arguments: %v\n", args[1:])
 		os.Exit(1)
 	}
 
 	log.Printf("Listening on: %v:%v (verbose %v)\n", config.Host, config.Port, config.Verbose)
+	log.Printf("Serving: %v\n", config.Dir)
 	for _, p := range config.Proxy {
 		log.Printf("Proxy: %v => %v\n", p.Path, p.To)
 	}
